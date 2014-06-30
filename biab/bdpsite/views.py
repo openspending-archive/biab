7747d99e733124ce891cd05457506d6d537292f9
Michael Bauer <mihi@lo-res.org>
2014-06-30 16:29:21 +0200

    django-recaptcha as a requirement
    We'll use it for the sign up form

--- biab/bdpsite/views.py
+++ biab/bdpsite/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render_to_response
 
 def welcome(request):
     return render_to_response("bdpsite/welcome.html")
+
+def createuser(request):
+    pass
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
