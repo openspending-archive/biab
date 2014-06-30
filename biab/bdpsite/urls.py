56bacb0009b976b6268f6ad3c30f6b591293ea4c
Michael Bauer <mihi@lo-res.org>
2014-06-30 16:04:40 +0200

    basic templating system

--- biab/bdpsite/urls.py
+++ biab/bdpsite/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'biab.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    
-    url(r'', include('bdpsite.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'bdpsite.views.welcome'),
 )
--- biab/biab/settings.py
+++ biab/biab/settings.py
@@ -25,6 +25,16 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'biab', 'templates'),
+    )
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    )
+
+
 ALLOWED_HOSTS = []
 
 
