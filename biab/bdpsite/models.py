ac4f707eb1ad049cae19e4237d4d264517ecde8a
Michael Bauer <mihi@lo-res.org>
2014-07-01 17:28:49 +0200

    added visualization data stuff

--- biab/bdpsite/models.py
+++ biab/bdpsite/models.py
@@ -46,3 +46,14 @@ class Dataset(models.Model):
 
 class Visualization(models.Model):
     dataset = models.ForeignKey(Dataset)
+    order = models.IntegerField(default=0)
+    drilldowns = models.CharField(max_length=512)
+    cuts = models.CharField(max_length=512)
+    type = models.CharField(max_length=20,
+        choices = (
+            ("bubbletree","Bubble Tree"),
+            ("barchart", "Bar Chart"),
+            ("treemap", "Tree Map"),
+            ("linebars" ,"Line Bars"),
+            ("stackedbar", "Stacked Bar Charts"),
+            ))
